e7c176b16d864356f487703eb5dd879baf1a6a78
이승희 <iseunghui@iseunghuiui-MacBookPro.local>
2023-11-19 22:40:59 +0900

    init

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by 이승희 on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
